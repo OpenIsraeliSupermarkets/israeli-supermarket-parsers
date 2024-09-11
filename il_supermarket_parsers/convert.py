71522a15db1c189c110a978cc0ff88af9d221e2a
Erlich Sefi <erlichsefi@gmail.com>
2024-09-11 20:06:05 +0000

    .

--- il_supermarket_parsers/convert.py
+++ il_supermarket_parsers/convert.py
@@ -15,6 +15,7 @@ class StoreParseingPipeline:
         self.database = MongoDb(self.store_enum.name)
 
     def process(self):
+        parser = ParserFactory.get(self.store_enum.name)
         data = DataLoader(self.folder, store_names=[self.store_enum.name])
 
     def convert(self, full_path, file_type, update_date):
