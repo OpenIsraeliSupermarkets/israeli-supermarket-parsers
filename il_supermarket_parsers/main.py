c2ffadb26754e5913cb12b8a2faa52992bd6e8ff
Erlich Sefi <erlichsefi@gmail.com>
2024-09-15 19:00:45 +0000

    clean

--- il_supermarket_parsers/main.py
+++ il_supermarket_parsers/main.py
@@ -6,16 +6,14 @@ class ConvertingTask:
     """main convert task"""
 
     def __call__(
-        self, data_folder="dumps", folders_to_process=None, number_of_processes=6
+        self, data_folder="dumps", number_of_processes=6
     ):
         Logger.info(
             f"Starting Parser, data_folder={data_folder},"
-            f"folders_to_process={folders_to_process}, "
             f"number_of_processes={number_of_processes}"
         )
         files_parsed = ParallelParser(
             data_folder,
-            folder_to_process=folders_to_process,
             number_of_processes=number_of_processes,
         ).execute()
         Logger.info(f"Ending Parser, files_parsed={files_parsed}")
