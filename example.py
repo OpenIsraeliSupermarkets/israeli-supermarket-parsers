575bcdb6a1563e7dab0a45ccabffb53fa20ac15c
Sefi Erlich <erlichsefi@gmail.com>
2024-09-21 20:02:11 +0000

    .

--- example.py
+++ example.py
@@ -1,6 +1,12 @@
 from il_supermarket_parsers import ConvertingTask
-
+from il_supermarket_scarper import ScarpingTask
 if __name__ == "__main__":
+
+    ScarpingTask(
+        enabled_scrapers=None, #download one from each 
+        limit=1,
+        multiprocessing=2
+    )
     scraper = ConvertingTask(
         dump_folder_name="dumps",
         multiprocessing=2
