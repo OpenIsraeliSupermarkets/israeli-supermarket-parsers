1a163cbdf7d2784a40b338fa1aa6be6d668333aa
Sefi Erlich <erlichsefi@gmail.com>
2024-09-21 20:45:26 +0000

    multiprocessing fix

--- il_supermarket_parsers/multiprocess_pharser.py
+++ il_supermarket_parsers/multiprocess_pharser.py
@@ -6,7 +6,7 @@ from .utils import FileTypesFilters
 import itertools
 
 
-class UploadRawToKaggle(ProcessJob):
+class RawProcessing(ProcessJob):
     """converting file to database"""
 
     def job(self, **kwargs):
@@ -25,19 +25,19 @@ class UploadRawToKaggle(ProcessJob):
 class ParallelParser(MultiProcessor):
     """run insert task on parallel"""
 
-    def __init__(self, data_folder, number_of_processes=6):
-        super().__init__(number_of_processes=number_of_processes)
+    def __init__(self, data_folder, multiprocessing=6):
+        super().__init__(multiprocessing=multiprocessing)
         self.data_folder = data_folder
 
     def task_to_execute(self):
         """the task to execute"""
-        return UploadRawToKaggle
+        return RawProcessing
 
     def get_arguments_list(self):
         """create list of arguments"""
 
-        all_parsers = ParserFactory.all_listed_parsers()
-        all_file_types = FileTypesFilters.all_types()
+        all_parsers = ParserFactory.all_parsers_name()
+        all_file_types = FileTypesFilters.all_full_files()
         params_order = ["type", "file_type", "data_folder"]
         combinations = list(
             itertools.product(
@@ -45,6 +45,6 @@ class ParallelParser(MultiProcessor):
             )
         )
         task_can_executed_indepentlly = [
-            dict(zip([params_order], combo)) for combo in combinations
+            dict(zip(params_order, combo)) for combo in combinations
         ]
         return task_can_executed_indepentlly
