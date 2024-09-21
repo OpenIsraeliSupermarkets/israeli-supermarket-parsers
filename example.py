5aa1d75459985fa28b5a248839cc54d7cc7d33a2
Sefi Erlich <erlichsefi@gmail.com>
2024-09-21 20:06:23 +0000

    .

--- example.py
+++ example.py
@@ -1,9 +1,11 @@
 from il_supermarket_parsers import ConvertingTask
-from il_supermarket_scarper import ScarpingTask
+from il_supermarket_scarper import ScarpingTask,ScraperFactory
 if __name__ == "__main__":
 
+
+
     ScarpingTask(
-        enabled_scrapers=None, #download one from each 
+        enabled_scrapers=[ScraperFactory.BAREKET.name], #download one from each 
         limit=1,
         multiprocessing=2
     ).start()
--- il_supermarket_parsers/__init__.py
+++ il_supermarket_parsers/__init__.py
@@ -1,3 +1,3 @@
-from main import ConvertingTask
+from .main import ConvertingTask
 from .parser_factroy import ParserFactory
 from .utils import FileTypesFilters
