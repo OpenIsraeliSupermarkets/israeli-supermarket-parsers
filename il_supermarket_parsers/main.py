988aac52daf5b69d5fd28a466d5bed77c1e35b91
Sefi Erlich <erlichsefi@gmail.com>
2024-09-21 18:36:25 +0000

    .

--- il_supermarket_parsers/main.py
+++ il_supermarket_parsers/main.py
@@ -5,20 +5,22 @@ from .utils.logger import Logger
 class ConvertingTask:
     """main convert task"""
 
-    def __call__(
+    def __init__(
         self, data_folder="dumps", number_of_processes=6
     ):
         Logger.info(
             f"Starting Parser, data_folder={data_folder},"
             f"number_of_processes={number_of_processes}"
         )
-        files_parsed = ParallelParser(
+        self.runner = ParallelParser(
             data_folder,
             number_of_processes=number_of_processes,
-        ).execute()
-        Logger.info(f"Ending Parser, files_parsed={files_parsed}")
-        return files_parsed
+        )
 
+    def start(self):
+        """run the parsing"""
+        return self.runner.execute()
+    
 
 if __name__ == "__main__":
     ConvertingTask()
