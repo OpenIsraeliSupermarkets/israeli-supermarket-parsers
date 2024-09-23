36312f73e0e4e8fd0ff06f5458a7c886befdc583
Sefi Erlich <erlichsefi@gmail.com>
2024-09-23 15:49:45 +0000

    .

--- il_supermarket_parsers/main.py
+++ il_supermarket_parsers/main.py
@@ -6,17 +6,20 @@ class ConvertingTask:
     """main convert task"""
 
     def __init__(
-        self, data_folder="dumps",enabled_parsers=None, multiprocessing=6, output_folder="output"
+        self, data_folder="dumps",enabled_parsers=None,files_types=None, multiprocessing=6, output_folder="output"
     ):
         Logger.info(
             f"Starting Parser, data_folder={data_folder},"
             f"number_of_processes={multiprocessing}"
             f"parsers = {enabled_parsers}"
+            f"files_types = {files_types}"
             f"output_folder={output_folder}"
+
         )
         self.runner = ParallelParser(
             data_folder,
             enabled_parsers=enabled_parsers,
+            enabled_file_types=files_types,
             multiprocessing=multiprocessing,
             output_folder=output_folder
         )
