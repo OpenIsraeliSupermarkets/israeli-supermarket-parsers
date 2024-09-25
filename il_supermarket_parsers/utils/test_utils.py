e4aad9f480d693a192648efbfd462b037fa20373
Sefi Erlich <erlichsefi@gmail.com>
2024-09-25 15:38:10 +0000

    reduce sample data

--- il_supermarket_parsers/utils/test_utils.py
+++ il_supermarket_parsers/utils/test_utils.py
@@ -2,7 +2,7 @@ import os
 from il_supermarket_scarper import ScarpingTask, FileTypesFilters, ScraperFactory
 
 
-def get_sample_data(dump_folder_name, filter_type=None, enabled_scrapers=None, limit=2):
+def get_sample_data(dump_folder_name, filter_type=None, enabled_scrapers=None, limit=3):
     """get data to scrape"""
     if filter_type:
         task = ScarpingTask(
