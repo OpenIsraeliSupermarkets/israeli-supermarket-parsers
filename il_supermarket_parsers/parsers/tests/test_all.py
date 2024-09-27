978b70ca81eaaa4bad8c291f7602b51265c5993a
Sefi Erlich <erlichsefi@gmail.com>
2024-09-27 09:35:32 +0000

    clean

--- il_supermarket_parsers/parsers/tests/test_all.py
+++ il_supermarket_parsers/parsers/tests/test_all.py
@@ -7,228 +7,244 @@ class BareketTestCase(make_test_case(ScraperFactory.BAREKET, ParserFactory.BAREK
     """
     Test case for Bareket supermarket.
     """
-    pass
 
 
-class YaynotBitanTestCase(make_test_case(ScraperFactory.YAYNO_BITAN, ParserFactory.YAYNO_BITAN)):
+class YaynotBitanTestCase(
+    make_test_case(ScraperFactory.YAYNO_BITAN, ParserFactory.YAYNO_BITAN)
+):
     """
     Test case for Yaynot Bitan supermarket.
     """
-    pass
 
 
 class CofixTestCase(make_test_case(ScraperFactory.COFIX, ParserFactory.COFIX)):
     """
     Test case for Cofix supermarket.
     """
-    pass
 
 
 class DorAlonTestCase(make_test_case(ScraperFactory.DOR_ALON, ParserFactory.DOR_ALON)):
     """
     Test case for Dor Alon supermarket.
     """
-    pass
 
 
-class GoodPharmTestCase(make_test_case(ScraperFactory.GOOD_PHARM, ParserFactory.GOOD_PHARM)):
+class GoodPharmTestCase(
+    make_test_case(ScraperFactory.GOOD_PHARM, ParserFactory.GOOD_PHARM)
+):
     """
     Test case for Good Pharm supermarket.
     """
-    pass
 
 
-class HaziHinamTestCase(make_test_case(ScraperFactory.HAZI_HINAM, ParserFactory.HAZI_HINAM)):
+class HaziHinamTestCase(
+    make_test_case(ScraperFactory.HAZI_HINAM, ParserFactory.HAZI_HINAM)
+):
     """
     Test case for Hazi Hinam supermarket.
     """
-    pass
 
 
-class HetCohenTestCase(make_test_case(ScraperFactory.HET_COHEN, ParserFactory.HET_COHEN)):
+class HetCohenTestCase(
+    make_test_case(ScraperFactory.HET_COHEN, ParserFactory.HET_COHEN)
+):
     """
     Test case for Het Cohen supermarket.
     """
-    pass
 
 
 class KeshetTestCase(make_test_case(ScraperFactory.KESHET, ParserFactory.KESHET)):
     """
     Test case for Keshet supermarket.
     """
-    pass
 
 
-class KingStoreTestCase(make_test_case(ScraperFactory.KING_STORE, ParserFactory.KING_STORE)):
+class KingStoreTestCase(
+    make_test_case(ScraperFactory.KING_STORE, ParserFactory.KING_STORE)
+):
     """
     Test case for King Store supermarket.
     """
-    pass
 
 
-class Maayan2000TestCase(make_test_case(ScraperFactory.MAAYAN_2000, ParserFactory.MAAYAN_2000)):
+class Maayan2000TestCase(
+    make_test_case(ScraperFactory.MAAYAN_2000, ParserFactory.MAAYAN_2000)
+):
     """
     Test case for Maayan 2000 supermarket.
     """
-    pass
 
 
-class MahsaniAShukTestCase(make_test_case(ScraperFactory.MAHSANI_ASHUK, ParserFactory.MAHSANI_ASHUK)):
+class MahsaniAShukTestCase(
+    make_test_case(ScraperFactory.MAHSANI_ASHUK, ParserFactory.MAHSANI_ASHUK)
+):
     """
     Test case for Mahsani AShuk supermarket.
     """
-    pass
 
 
 class MegaTestCase(make_test_case(ScraperFactory.MEGA, ParserFactory.MEGA)):
     """
     Test case for Mega supermarket.
     """
-    pass
 
 
-class NetivHasefTestCase(make_test_case(ScraperFactory.NETIV_HASED, ParserFactory.NETIV_HASED)):
+class NetivHasefTestCase(
+    make_test_case(ScraperFactory.NETIV_HASED, ParserFactory.NETIV_HASED)
+):
     """
     Test case for Netiv Hased supermarket.
     """
-    pass
 
 
-class MeshnatYosef1TestCase(make_test_case(ScraperFactory.MESHMAT_YOSEF_1, ParserFactory.MESHMAT_YOSEF_1)):
+class MeshnatYosef1TestCase(
+    make_test_case(ScraperFactory.MESHMAT_YOSEF_1, ParserFactory.MESHMAT_YOSEF_1)
+):
     """
     Test case for Meshnat Yosef 1 supermarket.
     """
-    pass
 
 
-class MeshnatYosef2TestCase(make_test_case(ScraperFactory.MESHMAT_YOSEF_2, ParserFactory.MESHMAT_YOSEF_2)):
+class MeshnatYosef2TestCase(
+    make_test_case(ScraperFactory.MESHMAT_YOSEF_2, ParserFactory.MESHMAT_YOSEF_2)
+):
     """
     Test case for Meshnat Yosef 2 supermarket.
     """
-    pass
 
 
 class OsheradTestCase(make_test_case(ScraperFactory.OSHER_AD, ParserFactory.OSHER_AD)):
     """
     Test case for Osher Ad supermarket.
     """
-    pass
 
 
 class PolizerTestCase(make_test_case(ScraperFactory.POLIZER, ParserFactory.POLIZER)):
     """
     Test case for Polizer supermarket.
     """
-    pass
 
 
-class RamiLevyTestCase(make_test_case(ScraperFactory.RAMI_LEVY, ParserFactory.RAMI_LEVY)):
+class RamiLevyTestCase(
+    make_test_case(ScraperFactory.RAMI_LEVY, ParserFactory.RAMI_LEVY)
+):
     """
     Test case for Rami Levy supermarket.
     """
-    pass
 
 
-class SalachDabachTestCase(make_test_case(ScraperFactory.SALACH_DABACH, ParserFactory.SALACH_DABACH)):
+class SalachDabachTestCase(
+    make_test_case(ScraperFactory.SALACH_DABACH, ParserFactory.SALACH_DABACH)
+):
     """
     Test case for Salach Dabach supermarket.
     """
-    pass
 
 
-class ShefaBarcartAshemTestCase(make_test_case(ScraperFactory.SHEFA_BARCART_ASHEM, ParserFactory.SHEFA_BARCART_ASHEM)):
+class ShefaBarcartAshemTestCase(
+    make_test_case(
+        ScraperFactory.SHEFA_BARCART_ASHEM, ParserFactory.SHEFA_BARCART_ASHEM
+    )
+):
     """
     Test case for Shefa Barcart Ashem supermarket.
     """
-    pass
 
 
-class ShufersalTestCase(make_test_case(ScraperFactory.SHUFERSAL, ParserFactory.SHUFERSAL)):
+class ShufersalTestCase(
+    make_test_case(ScraperFactory.SHUFERSAL, ParserFactory.SHUFERSAL)
+):
     """
     Test case for Shufersal supermarket.
     """
-    pass
 
 
-class ShukAhirTestCase(make_test_case(ScraperFactory.SHUK_AHIR, ParserFactory.SHUK_AHIR)):
+class ShukAhirTestCase(
+    make_test_case(ScraperFactory.SHUK_AHIR, ParserFactory.SHUK_AHIR)
+):
     """
     Test case for Shuk Ahir supermarket.
     """
-    pass
 
 
-class StopMarketTestCase(make_test_case(ScraperFactory.STOP_MARKET, ParserFactory.STOP_MARKET)):
+class StopMarketTestCase(
+    make_test_case(ScraperFactory.STOP_MARKET, ParserFactory.STOP_MARKET)
+):
     """
     Test case for Stop Market supermarket.
     """
-    pass
 
 
-class SuperPharmTestCase(make_test_case(ScraperFactory.SUPER_PHARM, ParserFactory.SUPER_PHARM)):
+class SuperPharmTestCase(
+    make_test_case(ScraperFactory.SUPER_PHARM, ParserFactory.SUPER_PHARM)
+):
     """
     Test case for Super Pharm supermarket.
     """
-    pass
 
 
-class SuperYudaTestCase(make_test_case(ScraperFactory.SUPER_YUDA, ParserFactory.SUPER_YUDA)):
+class SuperYudaTestCase(
+    make_test_case(ScraperFactory.SUPER_YUDA, ParserFactory.SUPER_YUDA)
+):
     """
     Test case for Super Yuda supermarket.
     """
-    pass
 
 
-class SuperSapirTestCase(make_test_case(ScraperFactory.SUPER_SAPIR, ParserFactory.SUPER_SAPIR)):
+class SuperSapirTestCase(
+    make_test_case(ScraperFactory.SUPER_SAPIR, ParserFactory.SUPER_SAPIR)
+):
     """
     Test case for Super Sapir supermarket.
     """
-    pass
 
 
-class FreshMarketAndSuperDoshTestCase(make_test_case(ScraperFactory.FRESH_MARKET_AND_SUPER_DOSH, ParserFactory.FRESH_MARKET_AND_SUPER_DOSH)):
+class FreshMarketAndSuperDoshTestCase(
+    make_test_case(
+        ScraperFactory.FRESH_MARKET_AND_SUPER_DOSH,
+        ParserFactory.FRESH_MARKET_AND_SUPER_DOSH,
+    )
+):
     """
     Test case for Fresh Market and Super Dosh supermarket.
     """
-    pass
 
 
 class QuikTestCase(make_test_case(ScraperFactory.QUIK, ParserFactory.QUIK)):
     """
     Test case for Quik supermarket.
     """
-    pass
 
 
 class TivTaamTestCase(make_test_case(ScraperFactory.TIV_TAAM, ParserFactory.TIV_TAAM)):
     """
     Test case for Tiv Taam supermarket.
     """
-    pass
 
 
 class VictoryTestCase(make_test_case(ScraperFactory.VICTORY, ParserFactory.VICTORY)):
     """
     Test case for Victory supermarket.
     """
-    pass
 
 
 class YellowTestCase(make_test_case(ScraperFactory.YELLOW, ParserFactory.YELLOW)):
     """
     Test case for Yellow convenience store.
     """
-    pass
 
 
-class YohananofTestCase(make_test_case(ScraperFactory.YOHANANOF, ParserFactory.YOHANANOF)):
+class YohananofTestCase(
+    make_test_case(ScraperFactory.YOHANANOF, ParserFactory.YOHANANOF)
+):
     """
     Test case for Yohananof supermarket.
     """
-    pass
 
 
-class ZolVeBegadolTestCase(make_test_case(ScraperFactory.ZOL_VEBEGADOL, ParserFactory.ZOL_VEBEGADOL)):
+class ZolVeBegadolTestCase(
+    make_test_case(ScraperFactory.ZOL_VEBEGADOL, ParserFactory.ZOL_VEBEGADOL)
+):
     """
     Test case for Zol VeBegadol supermarket.
     """
-    pass
