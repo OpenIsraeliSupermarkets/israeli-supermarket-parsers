4fe9ee5af6d7c4e34d60f38fbe1e55f2181be8af
Sefi Erlich <erlichsefi@gmail.com>
2024-10-01 21:23:21 +0000

    test

--- il_supermarket_parsers/documents/tests/test_xml_dataframe.py
+++ il_supermarket_parsers/documents/tests/test_xml_dataframe.py
@@ -1,22 +1,26 @@
 from il_supermarket_parsers.documents.xml_dataframe_parser import XmlDataFrameConverter
 
 
-def test_read_bad_encoding():
+def test_read_bad_encoding_1():
+    """test reading files that are the encoding in the file is not correct"""
 
     converter = XmlDataFrameConverter(list_key="Details", id_field="ItemCode")
     df = converter.convert(
         "il_supermarket_parsers/documents/tests",
         "PriceFull7290172900007-083-202409270311.xml",
     )
+    converter.validate_succussful_extraction(df,"il_supermarket_parsers/documents/tests/PriceFull7290172900007-083-202409270311.xml")
+
     assert df.shape[0] > 0
 
 
-
-def test_read_bad_encoding1():
+def test_read_bad_encoding_2():
+    """test reading files that are the encoding in the file is not correct"""
 
     converter = XmlDataFrameConverter(list_key="Details", id_field="ItemCode")
     df = converter.convert(
         "il_supermarket_parsers/documents/tests",
         "PromoFull7290172900007-667-202409290706.xml",
     )
+    converter.validate_succussful_extraction(df,"il_supermarket_parsers/documents/tests/PromoFull7290172900007-667-202409290706.xml")
     assert df.shape[0] > 0
