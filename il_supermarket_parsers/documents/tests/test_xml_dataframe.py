8d3953850cc345e61c2707761c5d815134b5e1de
Sefi Erlich <erlichsefi@gmail.com>
2024-10-01 21:20:08 +0000

    testcases

--- il_supermarket_parsers/documents/tests/test_xml_dataframe.py
+++ il_supermarket_parsers/documents/tests/test_xml_dataframe.py
@@ -9,3 +9,14 @@ def test_read_bad_encoding():
         "PriceFull7290172900007-083-202409270311.xml",
     )
     assert df.shape[0] > 0
+
+
+
+def test_read_bad_encoding1():
+
+    converter = XmlDataFrameConverter(list_key="Details", id_field="ItemCode")
+    df = converter.convert(
+        "il_supermarket_parsers/documents/tests",
+        "PromoFull7290172900007-667-202409290706.xml",
+    )
+    assert df.shape[0] > 0
