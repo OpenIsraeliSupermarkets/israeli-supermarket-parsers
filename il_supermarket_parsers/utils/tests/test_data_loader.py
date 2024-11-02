96f869882967586495cd4301859d517a221cbb58
Sefi Erlich <erlichsefi@gmail.com>
2024-11-02 15:01:39 +0200

    .

--- il_supermarket_parsers/utils/tests/test_data_loader.py
+++ il_supermarket_parsers/utils/tests/test_data_loader.py
@@ -2,5 +2,6 @@ from il_supermarket_parsers.utils.data_loader import DataLoader
 
 
 def test_load_null_data():
+    """Test loading null data"""
     data = DataLoader("il_supermarket_parsers/utils/tests").load()
     assert len(data) == 3
