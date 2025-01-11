d2af8850adc5988d50ba8f08249f19ad034b43b5
Sefi Erlich <erlichsefi@gmail.com>
2025-01-11 14:00:39 +0200

    .

--- il_supermarket_parsers/parsers/tests/test_case.py
+++ il_supermarket_parsers/parsers/tests/test_case.py
@@ -9,6 +9,7 @@ from il_supermarket_parsers.utils import (
     EMPTY_FILE_TOEHOLD,
 )
 from il_supermarket_parsers.parser_factory import ParserFactory
+from il_supermarket_scarper import ScraperFactory
 
 
 def make_test_case(scraper_enum, parser_enum):
@@ -55,14 +56,16 @@ def make_test_case(scraper_enum, parser_enum):
             )
 
         def _parser_validate(self, file_type):
-
+            """test the sub case"""
             with tempfile.TemporaryDirectory() as tmpdirname:
                 self.__parser_validate(file_type, tmpdirname)
 
         def __parser_validate(self, file_type, dump_path="temp"):
             """test the sub case"""
             sub_folder = self._get_temp_folder(dump_path)
-            self._refresh_download_folder(sub_folder, file_type)
+            
+            if ScraperFactory.is_scraper_enabled(self.scraper_enum):
+                self._refresh_download_folder(sub_folder, file_type)
 
             parser = self.parser_class()
 
